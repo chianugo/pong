75fc9c3c8f13cdbd90251acf298141d31403c9ff
cko003 <cko003@bucknell.edu>
2024-02-08 17:07:03 +0100

    implement player paddle logic and color change on bounce

--- Ball.js
+++ Ball.js
@@ -1,4 +1,5 @@
 const INITIAL_VELCITY = 0.025;
+const VELCOITY_INCREASE = 0.00001;
 export default class Ball {
   constructor(ballElement) {
     this.ballElement = ballElement;
@@ -48,14 +49,23 @@ export default class Ball {
   update(delta) {
     this.x += this.direction.x * this.velocity * delta;
     this.y += this.direction.y * this.velocity * delta;
+    this.velocity = Math.min(this.velocity + VELCOITY_INCREASE * delta, 7); // div by 2 causes trailing
     const rect = this.rect();
 
     if (rect.bottom >= window.innerHeight || rect.top <= 0) {
       this.direction.y *= -1;
+      document.documentElement.style.setProperty(
+        "--hue",
+        randomNumberBetween(0, 360)
+      );
     }
 
     if (rect.right >= window.innerWidth || rect.left <= 0) {
       this.direction.x *= -1;
+      document.documentElement.style.setProperty(
+        "--hue",
+        randomNumberBetween(0, 360)
+      );
     }
   }
 }
---
+++ Paddle.js
@@ -0,0 +1,15 @@
+export default class Paddle {
+  constructor(paddleElement) {
+    this.paddleElement = paddleElement;
+  }
+
+  get position() {
+    return parseFloat(
+      getComputedStyle(this.paddleElement).getPropertyValue("--position")
+    );
+  }
+
+  set position(value) {
+    this.paddleElement.style.setProperty("--position", value);
+  }
+}
